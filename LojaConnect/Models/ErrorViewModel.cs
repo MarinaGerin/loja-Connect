d6ee2829d82fa92229a2954d9f1c9e57b83233ab
MarinaGerin <mgerin2211@gmail.com>
2025-02-06 17:06:54 -0300

    att

--- LojaConnect/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LojaConnect.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ banco de dados/banco de dados script.sql
@@ -0,0 +1,36 @@
+-- criar banco
+create database loja; 
+
+use loja;
+
+create table Categoria 
+(
+	Id			int not null identity primary key,
+	Nome		varchar(50) not null unique,
+	Foto		varchar(200),
+	ExibirHome  bit
+);
+
+create table Produto
+(
+	Id			int not null identity primary key,
+	Nome		varchar(100) not null,
+	Descricao	varchar(1000),
+	CategoriaId	int not null,
+	ValorCusto	numeric(10,2),
+	ValorVenda	numeric(10,2) not null,
+	QtdeEstoque int not null default 0,
+	Destaque	bit default 0,
+	Foreign key	(CategoriaId) references Categoria(Id)
+
+);
+
+create table ProdutoFoto
+(
+	Id			int not null identity primary key,
+	ProdutoId	int not null,
+	Foto		varchar(200) not null,
+	Descricao	varchar(100)
+	foreign key (ProdutoId) references Produto(Id)
+
+);
